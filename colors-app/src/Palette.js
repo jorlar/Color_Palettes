c26154b7bd40d64cbb14d9686cd677f9710a0616
jorlar <jqern.larsen@gmail.com>
2022-07-09 11:45:13 +0200

    :art: Added basic CSS and ColorBox

--- colors-app/src/Palette.js
+++ colors-app/src/Palette.js
@@ -1,10 +1,19 @@
 import React, {Component} from 'react'
+import ColorBox from './ColorBox'
+import './Palette.css'
 
 export default class Palette extends Component {
     render() {
+        const colorBoxes = this.props.colors.map(color => (
+            <ColorBox background={color.color} name={color.name} />
+        ));
         return (
-            <div>
-                <h1>PALETTE</h1>
+            <div className='Palette'>
+                {/*Navbar in here*/}
+                <div className='Palette-colors'>
+                {colorBoxes}
+                </div>
+                {/*Footer*/}
             </div>
         )
     }
